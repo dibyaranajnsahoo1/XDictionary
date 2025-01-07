5cb122def4853c9cb716d83d4d1110a372754cf3
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2025-01-07 16:11:26 +0530

    added

---
+++ x-dictionary/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Xdictionary from './Components/Xdictionary';
+
+function App() {
+  return (
+    <div className="App">
+     < Xdictionary/>
+    </div>
+  );
+}
+
+export default App;
